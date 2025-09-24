430c25ace0e14db6bbd9f7be22ccd50b6db4876d
Vadim bikmukhametov <streetracer.56rus@gmail.com>
2025-09-24 22:54:31 +0500

    Исправление в одну строчку

--- 6-driver/app.js
+++ 6-driver/app.js
@@ -1,5 +1,1 @@
-const hasLicence = true;
-const age = 90;
-const isDrunk = false;
-
-console.log(`${hasLicence && age>=18 && !isDrunk ? 'Пользователь может водить авто' : 'Пользователь не может водить авто'}`);
+const canDrive = (hasLicence, age, isDrunk) => age > 18 && hasLicence && !isDrunk ? 'может' : 'не может';
