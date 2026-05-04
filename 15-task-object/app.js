bac72594753acc7fcf62a57e97667777fc3ac567
Vadim bikmukhametov <streetracer.56rus@gmail.com>
2026-05-04 23:06:52 +0500

    Исправление коммита по 15 заданию

--- 15-task-object/app.js
+++ 15-task-object/app.js
@@ -1,10 +1,30 @@
-const searchParameters = {
-  search: ‘Вася’,
-  take: 10
-}
+const toDoList = {
+  tasks: [ 
+  { title: 'Помыть посуду', id: 1, priority: 1,}
+  ],
 
-function querySearch(searchParameters) {
-  const keys = Object.keys(searchParameters);
-  const params = keys.map(key => `${key}=${searchParameters[key]}`);
-  return params.join('&');
+  addTask: function(title, id, priority) {
+    this.tasks.push({title, id, priority});
+  },
+  deleteTaskById: function(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id)
+  },
+  updateTaskById: function(id, newTitle, newPriority) {
+    const task = this.tasks.find(task => task.id === id);
+
+    if (task) {
+      if (newTitle !== undefined) {
+        task.title = newTitle;
+      }
+
+      if (newPriority !== undefined) {
+        task.priority = newPriority;
+      }
+    }
+
+  },
+  sortByPriority: function() {
+    this.tasks.sort((a, b) => a.priority - b.priority);
+  }
+
 }
