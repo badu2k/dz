55334171c1bc5b4082110f2f956827c1381c2c71
Vadim bikmukhametov <streetracer.56rus@gmail.com>
2026-07-14 18:05:32 +0500

    Перезалив дз

--- 1-map/app.js
+++ 1-map/app.js
@@ -1,36 +1,8 @@
-const number1 = document.querySelector('.number1');
-const number2 = document.querySelector('.number2');
-const addition = document.querySelector('.addition');
-const subtraction = document.querySelector('.subtraction');
-const multiplication = document.querySelector('.multiplication');
-const division = document.querySelector('.division');
-const result = document.querySelector('.result');
-const clear = document.querySelector('.clear');
-addition.addEventListener('click', function(){
-    result.innerText = Number(number1.value) + Number(number2.value);
-    number1.value = "";
-    number2.value = "";
-  });
-
-subtraction.addEventListener('click', function(){
-    result.innerText = Number(number1.value) - Number(number2.value);
-    number1.value = "";
-    number2.value = "";
-  });
-
-multiplication.addEventListener('click', function(){
-    result.innerText = Number(number1.value) * Number(number2.value);
-    number1.value = "";
-    number2.value = "";
-  });
-
-division.addEventListener('click', function(){
-  result.innerText = Number(number1.value) / Number(number2.value);
-  number1.value = "";
-  number2.value = "";
-});
-
-clear.addEventListener('click', function(){
-  number1.value = "";
-  number2.value = "";
-});
+const arr = [
+  { id: 1, name: 'Вася' },
+  { id: 2, name: 'Петя' },
+  { id: 1, name: 'Вася' },
+];
+const uniqueIds = [...new Set(arr.map(item => item.id))];
+const unique = uniqueIds.map(id => arr.find(item => item.id === id));
+console.log(unique);
