9c5abd54295d9ed9e8205b7ac869120a4231e027
Vadim bikmukhametov <streetracer.56rus@gmail.com>
2026-07-31 18:03:12 +0500

    Перезалив

--- 6-Class/app.js
+++ 6-Class/app.js
@@ -1,40 +1,25 @@
-function Character(race, name, lang) {
-  this.race = race;
-  this.name = name;
-  this.lang = lang;
+class Car {
+  #make;
+  #model;
+  #mileage;
+  constructor(make, model, mileage ) {
+    this.#login = login;
+    this.#password = password;
+    this.#make =  make;
+    this.#model = model;
+    this.#mileage = mileage;
+  }
+
+  get mileage() {
+    return this.#mileage;
+  }
+
+  set mileage(newMileage) {
+    this.#mileage = newMileage;
+  }
+  
+  info() {
+    console.log(this.#make, this.#model, this.#mileage);
+  }
+
 }
-
-Character.prototype.speak = function() {
-  console.log(`${this.name} говорит на языке: ${this.lang}`);
-}
-function Orc (race, name, language, weapon) {
-  Character.call(this, race, name, language);
-  this.weapon = weapon;
-}
-
-Orc.prototype = Object.create(Character.prototype);
-Orc.prototype.constructor = Orc;
-Orc.prototype.hit = function() {
-  console.log(`${this.name} наносит удар оружием: ${this.weapon}`); 
-}
-
-function Elf(race, name, language, spellType) {
-  Character.call(this, race, name, language);
-  this.spellType = spellType;
-}
-
-Elf.prototype = Object.create(Character.prototype);
-Elf.prototype.constructor = Elf;
-
-Elf.prototype.createSpell = function () {
-  console.log(`${this.name} создает заклинание типа: ${this.spellType}`);
-};
-
-const orc = new Orc('Орк', 'Шрек', 'Орочий', 'Дубина');
-const elf = new Elf('Эльф', 'Ева', 'Эльфийский', 'Заморозка');
-
-orc.speak();
-orc.hit();
-
-elf.speak();
-elf.createSpell();
